751aba06d977e8306f3e462004bf33a308a292ac
navi9000 <icdi7434@gmail.com>
2025-12-25 19:25:09 +0300

    not found styles

--- src/pages/not-found/ui/NotFoundPage.tsx
+++ src/pages/not-found/ui/NotFoundPage.tsx
@@ -1,7 +1,19 @@
 import type { FC } from "react"
+import { Container } from "@/shared/ui"
+import { Link } from "react-router"
+import styles from "./NotFoundPage.module.css"
 
 const NotFoundPage: FC = () => {
-  return <div>NotFoundPage</div>
+  return (
+    <Container>
+      <Container.Child className={styles.content}>
+        Страница не найдена.{" "}
+        <Link to="/" className={styles.link}>
+          Вернуться на главную
+        </Link>
+      </Container.Child>
+    </Container>
+  )
 }
 
 export default NotFoundPage
